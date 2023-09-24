d84d3e977e2ae5ab35a0f6bd40dff5f27815475a
Cameron <cameron.palmer@my.jcu.edu.au>
2023-09-24 21:01:02 +1000

    Add refactored files

--- prac_02/score.py
+++ prac_02/score.py
@@ -3,17 +3,30 @@ CP1404/CP5632 - Practical
 Broken program to determine score status
 """
 
+import random
+
 
 def main():
     score = float(input("Enter score: "))
+    result = get_result(score)
+    random_result = get_result(get_random())
+    print(f"User's result: {result}")
+    print(f"Random result: {random_result}")
+
+
+def get_result(score):
     if 90 <= score <= 100:
-        print("Excellent!")
-    elif 50 <= score <= 100:
-        print("Pass")
-    elif 0 <= score <= 100:
-        print("Bad")
+        return "Excellent!"
+    elif 50 <= score < 90:
+        return "Pass"
+    elif 0 <= score < 50:
+        return "Bad"
     else:
-        print("Invalid Input!")
+        return "Invalid Input!"
+
+
+def get_random():
+    return random.randint(0, 100)
 
 
 main()
---
+++ prac_02/score_menu.py
@@ -0,0 +1,14 @@
+display menu
+choice = input("Choice: ").upper()
+while choice != "Q":
+    if choice == "G"
+        score = int(input("Score: "))
+    elif choice == "P":
+        print()
+    elif choice == "S":
+        <do n-th task>
+    else:
+        print("Invalid Choice")
+    display menu
+    choice = input("Choice: ").upper()
+<do final thing, if needed>
